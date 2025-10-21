9de591a1d27dddcb32675249cd8b819f82a84a09
mvrthn <martin@agb.home.pl>
2025-10-21 20:33:49 +0200

    optymalizacja notReplace()

--- lab03/src/CodingBat.java
+++ lab03/src/CodingBat.java
@@ -30,27 +30,19 @@ public class CodingBat {
     }
 
     public static String notReplace(String str) {
-        List<Character> arr = str.chars()
-                .mapToObj(c -> (char) c)
-                .collect(Collectors.toList());
-        for(int i = 0; i < arr.size(); i++) {
-            if(arr.get(i) != 'i') {
-                continue;
-            }
-            if(i == arr.size() - 1 || arr.get(i + 1) != 's') {
-                continue;
-            }
-            if(i != arr.size() - 2 && Character.isLetter(arr.get(i + 2))) {
-                continue;
-            }
-            if(i == 0 || !Character.isLetter(arr.get(i - 1))) {
-                arr.addAll(i + 2, Arrays.asList(' ', 'n', 'o', 't'));
+        StringBuilder sb = new StringBuilder();
+        char prev = ' ';
+        for(int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+            if(c == 'i' && i + 1 != str.length() && str.charAt(i + 1) == 's' && !Character.isLetter(prev)
+                    && (i + 2 == str.length() || !Character.isLetter(str.charAt(i + 2)))) {
+                sb.append("is not");
                 i++;
             }
-        }
-        StringBuilder sb = new StringBuilder();
-        for(Character c : arr) {
-            sb.append(c);
+            else {
+                sb.append(c);
+            }
+            prev = c;
         }
         return sb.toString();
     }
